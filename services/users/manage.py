b0200d42d61ca04dccd3427cee275a8fe9ba4afb
Joseph Martinsen <joseph@martinsen.com>
2018-12-21 23:58:23 -0600

    Implemented code coverage to Users

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,4 +1,5 @@
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 
@@ -8,9 +9,20 @@ from project.api.models import User
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+
 
 @cli.command()
 def recreate_db():
+    """Recreate or create the DB"""
     db.drop_all()
     db.create_all()
     db.session.commit()
@@ -18,7 +30,7 @@ def recreate_db():
 
 @cli.command()
 def test():
-    """ Runs the tests without code coverage"""
+    """Runs the tests without code coverage"""
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
@@ -32,6 +44,22 @@ def seed_db():
     db.session.add(User(username='joseph', email="joseph@mail.com"))
     db.session.add(User(username='tristan', email="tristan@mail.org"))
     db.session.commit()
+
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == '__main__':

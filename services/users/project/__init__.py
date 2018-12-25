50b723b4f3156e41d0239b48b454dec4fb699f0f
Joseph Martinsen <joseph@martinsen.com>
2018-12-25 12:56:24 -0600

    Added dev env Setup for Client

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # instantiate the db
@@ -12,6 +13,9 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+
+    # Enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')

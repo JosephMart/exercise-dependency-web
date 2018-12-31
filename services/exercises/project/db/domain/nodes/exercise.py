5a6a2375e2a10818a09470e13c0c66afc01f2a4c
Tristan Partin <tristan.partin@gmail.com>
2018-12-31 13:29:38 -0600

    Added hint property

--- services/exercises/project/db/domain/nodes/exercise.py
+++ services/exercises/project/db/domain/nodes/exercise.py
@@ -14,13 +14,15 @@ class Exercise(Entity):
 
     question: str = None
     solution: str = None
+    hint: str = None
     created_at: datetime = None
     updated_at: datetime = None
 
-    def __init__(self, id: Optional[int, None], question: str, solution: str, created_at=datetime.now(), updated_at=datetime.now()):
+    def __init__(self, id: Optional[int, None], question: str, solution: str, hint: str, created_at=datetime.now(), updated_at=datetime.now()):
         Entity.__init__(self, id)
         self.question = question
         self.solution = solution
+        self.hint = hint
         self.created_at = datetime.strptime(created_at, date_fmt_str)
         self.updated_at = datetime.strptime(updated_at, date_fmt_str)
 
@@ -30,10 +32,11 @@ class Exercise(Entity):
             'id': self.id,
             'question': self.question,
             'solution': self.solution,
+            'hint': self.hint,
             'created_at': str(self.created_at),
             'updated_at': str(self.updated_at)
         }
 
     def deserialize(**kwargs) -> Exercise:
         """Deserialize a `dict` into an `Exercise` object"""
-        return Exercise(kwargs['id'], kwargs['question'], kwargs['solution'], kwargs['created_at'], kwargs['updated_at'])
+        return Exercise(kwargs['id'], kwargs['question'], kwargs['solution'], kwargs['hint'], kwargs['created_at'], kwargs['updated_at'])

7c56385880137b4009a511f5ef4f8ff6b1b90d03
Tristan Partin <tristan.partin@gmail.com>
2018-12-31 13:30:22 -0600

    Changed title to name in Topic

--- services/exercises/project/db/domain/nodes/topic.py
+++ services/exercises/project/db/domain/nodes/topic.py
@@ -8,16 +8,16 @@ class Topic(Entity):
     """
     Python representation of a Topic model.
 
-    `title`: title of the topic
+    `name`: name of the topic
     """
 
-    title: str = None
+    name: str = None
     created_at: datetime = None
     updated_at: datetime = None
 
-    def __init__(self, id: Optional[int, None], title: str, created_at=datetime.now(), updated_at=datetime.now()):
+    def __init__(self, id: Optional[int, None], name: str, created_at=datetime.now(), updated_at=datetime.now()):
         Entity.__init__(self, id)
-        self.title = title
+        self.name = name
         self.created_at = datetime.strptime(created_at, date_fmt_str)
         self.updated_at = datetime.strptime(updated_at, date_fmt_str)
 
@@ -25,11 +25,11 @@ class Topic(Entity):
         """Serialize a `Topic` object into a `dict`"""
         return {
             'id': self.id,
-            'title': self.title,
+            'name': self.name,
             'created_at': str(self.created_at),
             'updated_at': str(self.updated_at)
         }
 
     def deserialize(**kwargs) -> Topic:
         """Deserialize a `dict` into a `Topic` object"""
-        return Topic(kwargs['id'], kwargs['title'], kwargs['created_at'], kwargs['updated_at'])
+        return Topic(kwargs['id'], kwargs['name'], kwargs['created_at'], kwargs['updated_at'])

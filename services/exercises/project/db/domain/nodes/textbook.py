1a6a311df4f801e7cc3ee966a3fc07d9161f26e4
Tristan Partin <tristan.partin@gmail.com>
2018-12-31 13:38:06 -0600

    Add author_id from users service

--- services/exercises/project/db/domain/nodes/textbook.py
+++ services/exercises/project/db/domain/nodes/textbook.py
@@ -9,15 +9,18 @@ class Textbook(Entity):
     Python representation of a Textbook model.
 
     `title`: title of the book
+    `author_id`: ID from the `users` service
     """
 
     title: str = None
+    author_id: int = None
     created_at: datetime = None
     updated_at: datetime = None
 
-    def __init__(self, id: Optional[int, None], title: str, created_at=datetime.now(), updated_at=datetime.now()):
+    def __init__(self, id: Optional[int, None], author_id: int, title: str, created_at=datetime.now(), updated_at=datetime.now()):
         Entity.__init__(self, id)
         self.title = title
+        self.author_id = author_id
         self.created_at = datetime.strptime(created_at, date_fmt_str)
         self.updated_at = datetime.strptime(updated_at, date_fmt_str)
 
@@ -26,10 +29,11 @@ class Textbook(Entity):
         return {
             'id': self.id,
             'title': self.title,
+            'author_id': self.author_id,
             'created_at': str(self.created_at),
             'updated_at': str(self.updated_at)
         }
 
     def deserialize(**kwargs) -> Textbook:
         """Deserialize a `dict` into a `User`"""
-        return Textbook(kwargs['id'], kwargs['title'], kwargs['created_at'], kwargs['updated_at'])
+        return Textbook(kwargs['id'], kwargs['title'], kwargs['author_id'], kwargs['created_at'], kwargs['updated_at'])
